e69cc3dd2953a2941bbfafdaaf3fe1a2bd7489e6
Njongwe <153155364+Njongwe@users.noreply.github.com>
2026-04-02 12:40:03 -0400

    Rename S3 bucket resource and update bucket name

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "S3 Oliver-7628" {
-  bucket = "codepipeline-S3 Oliver-7628"
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "terraform-demo-oliver-2026"
 }
